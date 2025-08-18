a8cf752cbfb71676a3bd32e7e8b374a8e58f4cdb
Kavya Sri <kavyasrir1676@gmail.com>
2025-08-18 21:00:56 +0530

    Add files via upload

---
+++ python/18-08-2025(3).py
@@ -0,0 +1,10 @@
+#write program to print sum of first n natural numbers
+n=int(input("enter a number : "))
+s=0
+for i in range(1,n+1):
+    s+=i
+print(s)
+''' input : enter a number : 5
+output : 15
+input : enter a number : 4
+output : 10'''
